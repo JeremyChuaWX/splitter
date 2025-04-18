7e596f664d92b9027045e004d446e13a4ad41057
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-04-19 00:29:13 +0800

    feat: get item for group

--- src/trpc/routers/item.ts
+++ src/trpc/routers/item.ts
@@ -1,6 +1,7 @@
 import { item } from "@/db/schema";
 import { protectedProcedure, createTRPCRouter } from "..";
 import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const itemRouter = createTRPCRouter({
     create: protectedProcedure
@@ -17,5 +18,17 @@ export const itemRouter = createTRPCRouter({
                     name: input.name,
                 },
             });
+        }),
+    getForGroup: protectedProcedure
+        .input(
+            z.object({
+                groupId: z.string(),
+            }),
+        )
+        .query(async ({ ctx, input }) => {
+            return await ctx.db
+                .select()
+                .from(item)
+                .where(eq(item.groupId, input.groupId));
         }),
 });
