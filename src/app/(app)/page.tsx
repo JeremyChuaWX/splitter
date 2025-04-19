98c41be4cc227edbbeff7add5f4287c66ab9530a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-04-19 12:04:25 +0800

    feat: loading state

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,6 +2,7 @@ import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 import { Suspense } from "react";
 import { CreateGroupComponent } from "./create-group-component";
 import { GroupsComponent } from "./groups-component";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Page() {
     prefetch(trpc.group.getAll.queryOptions());
@@ -10,7 +11,15 @@ export default function Page() {
         <HydrateClient>
             <div className="flex flex-col gap-6 pt-24 w-2/3">
                 <HeaderComponent />
-                <Suspense fallback={<div>loading...</div>}>
+                <Suspense
+                    fallback={
+                        <div className="flex flex-col gap-4 w-full">
+                            <Skeleton className="w-full rounded-xl h-[74px]" />
+                            <Skeleton className="w-full rounded-xl h-[74px]" />
+                            <Skeleton className="w-full rounded-xl h-[74px]" />
+                        </div>
+                    }
+                >
                     <GroupsComponent />
                 </Suspense>
             </div>
