fc046c53f5d35f6138aec96cbe4c76384960a42e
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-04-25 11:45:37 +0800

    feat: add database direct url for schema migrations

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
     server: {
         DATABASE_URL: z.string().url(),
+        DATABASE_DIRECT_URL: z.string().url(),
         CLERK_SECRET_KEY: z.string(),
     },
     client: {
