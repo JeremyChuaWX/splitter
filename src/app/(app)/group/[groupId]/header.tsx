de887b2c115271ff1d767966dd4dc6302974690e
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-04-27 22:28:35 +0800

    feat: add back link

--- src/app/(app)/group/[groupId]/header.tsx
+++ src/app/(app)/group/[groupId]/header.tsx
@@ -4,6 +4,8 @@ import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { SettingsDropdown } from "./settings-dropdown";
 import { useParams } from "next/navigation";
+import Link from "next/link";
+import { LucideChevronLeft } from "lucide-react";
 
 export function Header() {
     const { groupId } = useParams<{ groupId: string }>();
@@ -14,7 +16,12 @@ export function Header() {
 
     return (
         <div className="flex gap-2 items-center w-full">
-            <h1 className="flex-1 text-2xl font-bold">{group.name}</h1>
+            <div className="flex-1">
+                <Link href="/" className="flex gap-1 items-center w-min">
+                    <LucideChevronLeft size={24} />
+                    <h1 className="text-2xl font-bold">{group.name}</h1>
+                </Link>
+            </div>
             <SettingsDropdown />
         </div>
     );
