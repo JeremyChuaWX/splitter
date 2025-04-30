ea80a864d0f8f6cce69bf1dbed2d162681a9af25
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-04-30 23:54:14 +0800

    feat: update loader

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -1,5 +1,30 @@
-import { Skeleton } from "@/components/ui/skeleton";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const DELAY = 300;
 
 export default function Loading() {
-    return <Skeleton className="w-full h-full" />;
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShow(true);
+        }, DELAY);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className="flex justify-center items-center">
+            <Loader2
+                size={24}
+                className={cn(
+                    "animate-spin transition-opacity duration-300 ease-in",
+                    show ? "opacity-100" : "opacity-0",
+                )}
+            />
+        </div>
+    );
 }
