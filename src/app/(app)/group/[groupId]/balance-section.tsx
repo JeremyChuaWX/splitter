c4678b825c1fe60515debf694833bb3812f3fa58
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-05-06 20:24:37 +0800

    feat: balance section

--- src/app/(app)/group/[groupId]/balance-section.tsx
+++ src/app/(app)/group/[groupId]/balance-section.tsx
@@ -1,26 +1,56 @@
+"use client";
+
 import { AddMembersButton } from "./add-members-button";
 import {
     Table,
     TableBody,
     TableCaption,
     TableCell,
-    TableFooter,
     TableRow,
 } from "@/components/ui/table";
+import { bigintToCurrency, cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+function textColor(balance: bigint) {
+    if (balance < 0n) {
+        return "text-red-600 dark:text-red-400";
+    } else if (balance === 0n) {
+        return "";
+    } else {
+        return "text-emerald-600 dark:text-emerald-400";
+    }
+}
 
 export function BalanceSection() {
+    const { groupId } = useParams<{ groupId: string }>();
+    const trpc = useTRPC();
+    const { data: balances } = useSuspenseQuery(
+        trpc.getBalances.queryOptions({ groupId: groupId }),
+    );
+
     return (
         <div className="flex flex-col gap-6">
             <h2 className="text-xl font-medium">Balances</h2>
             <Table>
-                <TableBody></TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TableCell className="text-right" colSpan={2}>
-                            $123.45
-                        </TableCell>
-                    </TableRow>
-                </TableFooter>
+                <TableBody>
+                    {balances.map((balance) => (
+                        <TableRow key={balance.id}>
+                            <TableCell className="font-medium">
+                                {balance.username}
+                            </TableCell>
+                            <TableCell
+                                className={cn(
+                                    "text-right",
+                                    textColor(balance.balance),
+                                )}
+                            >
+                                ${bigintToCurrency(balance.balance)}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
                 <TableCaption>
                     <AddMembersButton />
                 </TableCaption>
