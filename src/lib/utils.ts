677887aaf2db6d944ae99165ea29b7dabf213d7b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-05-06 20:15:53 +0800

    feat: handle negative values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,16 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function bigintToCurrency(n: bigint) {
+    const abs_n = n < 0 ? -n : n;
     const SCALE_FACTOR = 100n;
-    const whole = n / SCALE_FACTOR;
-    const decimal = n % SCALE_FACTOR;
+    const whole = abs_n / SCALE_FACTOR;
+    const decimal = abs_n % SCALE_FACTOR;
     const decimalString = decimal.toString().padStart(2, "0");
-    return `${whole}.${decimalString}`;
+    if (n < 0) {
+        return `(${whole}.${decimalString})`;
+    } else {
+        return `${whole}.${decimalString}`;
+    }
 }
 
 export function numberToBigint(n: number): bigint {
